efb687b3082a59d41c07e6bb2a22ddbde7b8e8f6
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2024-05-23 21:37:24 -0500

    clean up

--- SurfsUp/.ipynb_checkpoints/app-checkpoint.py
+++ SurfsUp/.ipynb_checkpoints/app-checkpoint.py
@@ -1,29 +1,32 @@
 # Import the dependencies.
+from flask import Flask, jsonify
 
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
